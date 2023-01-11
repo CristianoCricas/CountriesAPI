46a817d2f87eca7c93605383e7d37b3b294fea5d
Cristiano Cricas <souzacricas@gmail.com>
2023-01-11 00:15:24 -0300

    Adjusts on postgres_init script for docker

--- docker/postgresql/docker_postgres_init.sql
+++ docker/postgresql/docker_postgres_init.sql
@@ -2,6 +2,7 @@ CREATE TABLE "__EFMigrationsHistory" (
 	"MigrationId" character varying(150) NOT NULL,
 	"ProductVersion" character varying(32) NOT NULL,
 CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
 
 INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
 VALUES ('20230108142841_InitialDB', '7.0.1');
